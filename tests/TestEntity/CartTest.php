fd754e48aa5c4faa906194add18dcf2768fba9a2
hilderjares <hider1195@gmail.com>
2020-02-15 22:29:36 -0300

    testing if add products in cart

--- tests/TestEntity/CartTest.php
+++ tests/TestEntity/CartTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Entity\Cart;
+use App\Entity\Product;
 use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase
@@ -9,5 +11,18 @@ class CartTest extends TestCase
         $class = class_exists('\\App\\Entity\\Cart');
 
         $this->assertTrue($class);
+    }
+
+    public function testAddProductInCart()
+    {
+        $product = new Product();
+        $product->setName('Product 1');
+        $product->setPrice(20.10);
+        $product->setCategory('Category One');
+        
+        $cart = new Cart();
+        $cart->addProduct($product);
+
+        $this->assertIsArray($cart->getProducts());
     }
 }
