2f476e7c2ed54053edd16787ebe714fc1734564e
hilderjares <hider1195@gmail.com>
2020-02-15 22:31:44 -0300

    refactoring cart to pass in tests

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -6,12 +6,15 @@ namespace App\Entity;
 
 class Cart
 {
+    private $products = [];
+
     public function addProduct(Product $product)
     {
+        $this->products[] = $product;
     }
 
     public function getProducts(): array
     {
-        return [];
+        return $this->products;
     }
 }
