cf32679690e7325107f7a18fd6f9740303ccb9d2
hilderjares <hider1195@gmail.com>
2020-02-15 22:30:06 -0300

    make it pass

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -1,7 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 class Cart
 {
+    public function addProduct(Product $product)
+    {
+    }
+
+    public function getProducts(): array
+    {
+        return [];
+    }
 }
